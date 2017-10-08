0ef0f837be2639519c39db32738ef650fb043f64
Matthew Fasman <matthew.f4sm4n@gmail.com>
2017-10-08 03:02:04 -0400

    Serving build folder from server

--- src/express/expressHandler.js
+++ src/express/expressHandler.js
@@ -1,9 +1,9 @@
 const { log } = require('./../utility/logger');
+const express = require('express');
+const path = require('path');
 
 module.exports = Object.freeze({
   initExpress: (app) => {
-    // This is an endpoint just for testing that express is working
-    app.get('/:variable', (req, res) => res.send(`<h1>${req.url}</h1>`));
-    log('Test express using the endpoint "/:[variable]"');
+    app.use('/', express.static(path.join(__dirname, './../../canvas-app/build')));
   },
 });
